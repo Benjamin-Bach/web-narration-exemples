3c21b0e393d09efc9f1f5c166d345df403467d90
Benjamin-Bach <mail@benjaminbach.fr>
2020-12-09 17:22:53 +0100

    Work around horizontal scroll js

--- assets/js/horizontalScroll.js
+++ assets/js/horizontalScroll.js
@@ -3,6 +3,11 @@ document.querySelectorAll('.horizontal-scroll').forEach((item, i) => {
     let maxScrollLeft = item.scrollWidth - item.clientWidth;
     if((item.scrollLeft > 0) && (item.scrollLeft < maxScrollLeft)){
       e.preventDefault();
+      document.querySelector('html').scrollTo(0, item.offsetHeight)
+      console.log(item.scrollLeft)
+
+    }else{
+      console.log('out')
     }
     item.scrollLeft += e.deltaY * 30;
   })
